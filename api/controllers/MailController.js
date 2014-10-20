bf1eb8aa972e9319cd8bec6bd3e8a95cee2c2814
Angel Piña <six.aps@gmail.com>
2014-10-19 20:23:14 -0700

    initial commit

---
+++ api/controllers/MailController.js
@@ -0,0 +1,11 @@
+/**
+ * MailController
+ *
+ * @description :: Server-side logic for managing Mails
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
